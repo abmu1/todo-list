5f158b5508901d9ac668b4ed18543edd6283248b
abdi <126338923+abmu1@users.noreply.github.com>
2023-05-22 07:24:11 +0300

    .

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import { saveLibrary, getLibrary } from "./localStorage";
 saveLibrary(library());
 const Library = getLibrary() || library();
 
-console.log('hiiii')
-
 const defualtProject = projectFactory('Default');
 Library.addTodo(defualtProject);
 Library.currentTodo = defualtProject;
